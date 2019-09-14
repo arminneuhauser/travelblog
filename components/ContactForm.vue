5ba9f6e2c500a314b103548c159d9b1a41a26c04
Armin Neuhauser <aman@gmx.at>
2019-09-15 00:17:49 +0200

    test form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="contactform narrow margin-auto padding-bottom" v-editable="blok">
     <h1>{{ blok.headline }}</h1>
-    <!--<form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+    <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+      <input type="hidden" name="form-name" value="contact" />
       <p class="hidden">
         <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
-      </p>-->
-    <form name="contact" method="POST" data-netlify="true">
+      </p>
       <div class="text-field">
         <input type="text" name="name" placeholder="Dein Name">
         <label>Name</label>
