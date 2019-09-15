e3a47c5ebec3ee2fe3d39003f09f9a2d8dee44c5
Armin Neuhauser <aman@gmx.at>
2019-09-15 23:32:34 +0200

    add static 404 erro page

---
+++ pages/404.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container padding-bottom">
+    <div class="error">
+      <h1>404</h1>
+      <div>
+        <h2>Seite nicht gefunden</h2>
+        <p>Wir können die Seite, die du suchst, leider nicht finden.</p>
+        <nuxt-link to="/" class="button button--icon">
+          Zur Startseite
+          <svg class="rotate-270"><use xlink:href="#down"></use></svg>
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.error {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: baseline;
+
+  h1 {
+    font-family: $fs-sans;
+    font-size: 8rem;
+    margin: 40px 30px 5px 0;
+
+    @include breakpoint(l) {
+      font-size: 10rem;
+    }
+  }
+
+  h2 {
+    font-family: $fs-sans;
+    font-size: 3rem;
+    margin: 10px 0;
+    opacity: 0.5;
+
+    @include breakpoint(l) {
+      font-size: 5rem;
+    }
+  }
+
+  p {
+    font-size: 1.8rem;
+    margin-bottom: 30px;
+  }
+}
+</style>
