296245b6b5671e8deed304aabe6cc247075ea7ac
Armin Neuhauser <aman@gmx.at>
2019-09-16 00:11:49 +0200

    add contact success page

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,41 +1,29 @@
 <template>
-  <div class="contactform narrow margin-auto padding-bottom" v-editable="blok">
-    <h1>{{ blok.headline }}</h1>
-
-    <div class="alert alert--success" v-if="$route.hash === '#success'">
-      <p>Danke für deine Nachricht! Wir werden dir sobald wie möglich antworten.</p>
-      <nuxt-link class="close" :to="$route.path">
-        <svg><use xlink:href="#close"></use></svg>
-      </nuxt-link>
+  <div class="contactform" v-editable="blok">
+    <div class="container narrow padding-bottom">
+      <h1>{{ blok.headline }}</h1>
+      <form name="contact" method="POST" action="/kontakt/erfolg" netlify-honeypot="bot-field" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact">
+        <p class="hidden">
+          <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+        </p>
+        <div class="text-field">
+          <input type="text" name="name" placeholder="Dein Name">
+          <label>Name</label>
+        </div>
+        <div class="text-field">
+          <input type="email" name="email" placeholder="deine@email.at">
+          <label>E-Mail-Adresse</label>
+        </div>
+        <div class="text-field">
+          <textarea name="message" rows="5" placeholder="Deine Nachricht an uns"></textarea>
+          <label>Deine Nachricht</label>
+        </div>
+        <div>
+          <button type="submit" class="button button--large">Absenden</button>
+        </div>
+      </form>
     </div>
-
-    <form v-else name="contact" method="POST" action="#success" netlify-honeypot="bot-field" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact" />
-      <p class="hidden">
-        <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
-      </p>
-      <div class="text-field">
-        <input type="text" name="name" placeholder="Dein Name">
-        <label>Name</label>
-      </div>
-      <div class="text-field">
-        <input type="email" name="email" placeholder="deine@email.at">
-        <label>E-Mail-Adresse</label>
-      </div>
-      <div class="text-field">
-        <textarea name="message" rows="5" placeholder="Deine Nachricht an uns"></textarea>
-        <label>Deine Nachricht</label>
-      </div>
-      <!--<div class="radio">
-        <input id="radio" type="radio"><label for="radio">radio</label>
-      </div>
-      <div class="checkbox">
-        <input id="checkbox" type="checkbox"><label for="checkbox">checkbox</label>
-      </div>-->
-      <div>
-        <button type="submit" class="button button--large">Absenden</button>
-      </div>
-    </form>
   </div>
 </template>
 
---
+++ pages/kontakt/erfolg.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="container narrow padding-bottom padding-top">
+      <div class="alert alert--success">
+        <p>Danke für deine Nachricht! Wir werden dir sobald wie möglich antworten.</p>
+        <nuxt-link class="close" to="kontakt">
+          <svg><use xlink:href="#close"></use></svg>
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
