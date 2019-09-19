b04e9a1dc6da57f8859d4172b07eb014fec06ad1
Armin Neuhauser <aman@gmx.at>
2019-09-19 21:02:27 +0200

    fix contact form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,29 +1,27 @@
 <template>
-  <div class="contactform" v-editable="blok">
-    <div class="container narrow padding-bottom">
-      <h1>{{ blok.headline }}</h1>
-      <form name="contact" method="POST" action="/kontakt/erfolg" netlify-honeypot="bot-field" data-netlify="true">
-        <input type="hidden" name="form-name" value="contact">
-        <p class="hidden">
-          <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
-        </p>
-        <div class="text-field">
-          <input type="text" name="name" placeholder="Dein Name" required>
-          <label>Name</label>
-        </div>
-        <div class="text-field">
-          <input type="email" name="email" placeholder="deine@email.at" required>
-          <label>E-Mail-Adresse</label>
-        </div>
-        <div class="text-field">
-          <textarea name="message" rows="5" placeholder="Deine Nachricht an uns" required></textarea>
-          <label>Deine Nachricht</label>
-        </div>
-        <div>
-          <button type="submit" class="button button--large">Absenden</button>
-        </div>
-      </form>
-    </div>
+  <div class="contactform padding-bottom" v-editable="blok">
+    <h1>{{ blok.headline }}</h1>
+    <form name="contact" method="POST" action="/kontakt/erfolg" netlify-honeypot="bot-field" data-netlify="true">
+      <input type="hidden" name="form-name" value="contact">
+      <p class="hidden">
+        <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+      </p>
+      <div class="text-field">
+        <input type="text" name="name" placeholder="Dein Name" required>
+        <label>Name</label>
+      </div>
+      <div class="text-field">
+        <input type="email" name="email" placeholder="deine@email.at" required>
+        <label>E-Mail-Adresse</label>
+      </div>
+      <div class="text-field">
+        <textarea name="message" rows="5" placeholder="Deine Nachricht an uns" required></textarea>
+        <label>Deine Nachricht</label>
+      </div>
+      <div>
+        <button type="submit" class="button button--large">Absenden</button>
+      </div>
+    </form>
   </div>
 </template>
 
