4828fa07f45637f119f541a3cc473fddeee59331
Armin Neuhauser <aman@gmx.at>
2019-09-20 20:13:40 +0200

    fix cookie banner

--- components/CookieBanner.vue
+++ components/CookieBanner.vue
@@ -1,6 +1,6 @@
-<!-- stolen from https://github.com/EvodiaAut/vue-cookieconsent-component -->
 <template>
   <div>
+    <!-- stolen from https://github.com/EvodiaAut/vue-cookieconsent-component -->
     <div class="cookie-banner" v-if="show" role="dialog">
       <p>
         <strong>Jeder liebt Cookies</strong> 🍪<br>
@@ -16,58 +16,58 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        show: undefined,
-        cookieName: 'cookieconsent_status',
-        cookiePath: '/',
-        cookieDomain: '',
-        cookieExpiryDays: 365
-      }
+export default {
+  data () {
+    return {
+      show: undefined,
+      cookieName: 'cookieconsent_status',
+      cookiePath: '/',
+      cookieDomain: '',
+      cookieExpiryDays: 365
+    }
+  },
+  computed: {
+    cookie() {
+      return !this.getCookie(this.cookieName)
+    }
+  },
+  beforeMount () {
+    this.show = this.cookie
+  },
+  methods: {
+    dismiss () {
+      this.show = false
+      this.setCookie(this.cookieName, 1, this.cookieExpiryDays, this.cookieDomain, this.cookiePath)
     },
-    computed: {
-      cookie() {
-        return !this.getCookie(this.cookieName)
-      }
+    getCookie(name) {
+      const value = `; ${document.cookie}`
+      const parts = value.split(`; ${name}=`)
+
+      return parts.length !== 2 ?
+        undefined :
+        parts.pop().split(';').shift()
     },
-    beforeMount() {
-      this.show = this.cookie
-    },
-    methods: {
-      dismiss() {
-        this.show = false
-        this.setCookie(this.cookieName, 1, this.cookieExpiryDays, this.cookieDomain, this.cookiePath)
-      },
-      getCookie(name) {
-        const value = `; ${document.cookie}`
-        const parts = value.split(`; ${name}=`)
+    setCookie(name, value, expiryDays, domain, path) {
+      const exdate = new Date()
+      exdate.setDate(exdate.getDate() + (expiryDays || 365))
 
-        return parts.length !== 2 ?
-          undefined :
-          parts.pop().split(';').shift()
-      },
-      setCookie(name, value, expiryDays, domain, path) {
-        const exdate = new Date()
-        exdate.setDate(exdate.getDate() + (expiryDays || 365))
+      const cookie = [
+        `${name}=${value}`,
+        `expires=${exdate.toUTCString()}`,
+        `path=${(path || '/')}`
+      ]
 
-        const cookie = [
-          `${name}=${value}`,
-          `expires=${exdate.toUTCString()}`,
-          `path=${(path || '/')}`
-        ]
-
-        if (domain) {
-          cookie.push(`domain=${domain}`)
-        }
-
-        document.cookie = cookie.join(';')
+      if (domain) {
+        cookie.push(`domain=${domain}`)
       }
+
+      document.cookie = cookie.join(';')
     }
   }
+}
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .cookie-banner {
   position: fixed;
   bottom: 0;
