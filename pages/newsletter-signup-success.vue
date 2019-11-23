ffe56ede81dc57bd38d12494620b180c4ba5827f
Armin Neuhauser <aman@gmx.at>
2019-11-23 12:30:38 -0500

    add newsletter-signup success page

---
+++ pages/newsletter-signup-success.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <h1>Danke für deine Newsletter-Anmeldung</h1>
+    <div>
+      <p>Deine Anmeldung war erfolgreich.</p>
+      <nuxt-link to="/" class="button button--icon">
+        Zurück zur Startseite
+        <svg class="rotate-270"><use xlink:href="#down"></use></svg>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
