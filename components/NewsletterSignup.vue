c85e20641c994f1f17f7712ac47f1175a92aad14
Armin Neuhauser <aman@gmx.at>
2019-11-24 08:46:24 -0500

    change newsletter section and name of signup page

--- components/NewsletterSignup.vue
+++ components/NewsletterSignup.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="newsletter-signup">
     <div class="container">
-      <h1>
+      <h2>
         Gefallen dir unsere Beiträge?<br>
         Melde dich für unseren Newsletter an
-      </h1>
+      </h2>
       <!-- Begin Mailchimp Signup Form -->
       <div id="mc_embed_signup">
         <form action="https://solmates.us4.list-manage.com/subscribe/post?u=0da2999b05bab86e1496beaa8&amp;id=747fd43d15" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
@@ -15,7 +15,7 @@
               </div>
               <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
               <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_0da2999b05bab86e1496beaa8_747fd43d15" tabindex="-1" value=""></div>
-              <div class="clear"><input type="submit" value="Jetzt anmelden" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+              <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="button button--small">Anmelden</button>
             </div>
         </form>
       </div>
@@ -31,16 +31,36 @@
   text-align: center;
   padding: 70px 0;
 
-  h1 {
+  @include breakpoint(l) {
+    padding: 100px 0;
+  }
+
+  h2 {
     color: #fff;
     margin-top: 0;
     font-weight: normal;
   }
 
-  .text-field {
-    max-width: 400px;
-    margin-left: auto;
-    margin-right: auto;
+  #mc_embed_signup_scroll {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-bottom: 20px;
+
+    .text-field {
+      margin: 0;
+
+      input {
+        border: 0;
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+      }
+    }
+
+    button {
+      border-top-left-radius: 0;
+      border-bottom-left-radius: 0;
+    }
   }
 }
 </style>
