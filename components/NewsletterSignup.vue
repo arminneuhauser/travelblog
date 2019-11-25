7d56db92308a9d7f8eb008242bdb3f6692c25c28
Armin Neuhauser <aman@gmx.at>
2019-11-24 20:17:07 -0500

    improve signup form section

--- components/NewsletterSignup.vue
+++ components/NewsletterSignup.vue
@@ -9,10 +9,7 @@
       <div id="mc_embed_signup">
         <form action="https://solmates.us4.list-manage.com/subscribe/post?u=0da2999b05bab86e1496beaa8&amp;id=747fd43d15" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
             <div id="mc_embed_signup_scroll">
-              <div class="text-field">
-                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="E-Mail-Adresse" required>
-                <label for="mce-EMAIL">Deine E-Mail-Adresse</label>
-              </div>
+              <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Deine E-Mail-Adresse" required>
               <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
               <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_0da2999b05bab86e1496beaa8_747fd43d15" tabindex="-1" value=""></div>
               <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="button">
@@ -51,14 +48,19 @@
     justify-content: center;
     align-items: center;
 
-    .text-field {
-      margin: 0;
+    input {
+      border: 0;
+      border-radius: 4px 0 0 4px;
+      height: 42px;
+      padding: 10px 15px;
+      font-size: 1.6rem;
+      font-weight: 400;
+      font-family: $fs-sans;
+      flex-grow: 1;
+      max-width: 300px;
 
-      input {
-        border: 0;
-        border-top-right-radius: 0;
-        border-bottom-right-radius: 0;
-        height: 42px;
+      &:focus {
+        outline: none;
       }
     }
 
@@ -73,22 +75,29 @@
     }
 
     @include breakpoint(s, max) {
-      padding: 0 5px;
       background: #fff;
+      padding: 0 5px;
       height: 50px;
       border-radius: 25px;
       justify-content: space-between;
 
-      .text-field {
-        input {
-          background: none;
-        }
+      input {
+        background: none;
       }
 
       button {
         background: none;
         padding: 0;
         display: flex;
+        border-radius: 100%;
+
+        &:hover {
+          background: $cta;
+
+          svg {
+            fill: #fff;
+          }
+        }
 
         svg {
           display: block;
